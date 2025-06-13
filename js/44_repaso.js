6b8e3e3ef2d575138609cf114821a0611d2115c7
errefrkl <canvacanvacanvauser@gmail.com>
2025-06-13 17:11:08 +0200

    ejercicios viernes

---
+++ js/44_repaso.js
@@ -0,0 +1,34 @@
+
+// IMPRIMIR EN LA CONSOLA DEL 1 AL 20
+
+
+// for (var i = 1; i <= 20; i ++) { console.log (i)};
+
+// var limite =1;
+
+// while (limite <=20) {
+//     console.log ('While: ' + limite);
+//     limite += 1;
+    
+// }
+
+
+// bucle while ---> limite += 1; es el incremento
+
+// IMPRIMIR EN LA CONSOLA DEL 1 AL 20 en una sola fila  (Array)
+
+var lista= [];
+
+ for (var i = 1; i <= 20; i ++) { 
+   
+    lista.push (i);
+    
+}
+ console.log (lista);
+
+
+
+
+
+
+
